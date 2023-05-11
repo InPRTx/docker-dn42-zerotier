b5d4fb9332974c6b5090ec34057be4f6656bce01
InPRTx <inprtx@gmail.com>
2023-05-11 18:48:52 +0800

    fix job1 update_dn42_data 首次执行顺序

--- host_script/__main__.py
+++ host_script/__main__.py
@@ -222,8 +222,8 @@ async def job1() -> None:
 
 async def main():
     print('server wg pubkey:', pub_key)
-    await update_dn42_data(False)
     await job1()
+    await update_dn42_data(False)
     aio_scheduler.add_job(job1, 'interval', minutes=1)
     aio_scheduler.add_job(update_dn42_data, 'interval', minutes=15)
     aio_scheduler.start()
