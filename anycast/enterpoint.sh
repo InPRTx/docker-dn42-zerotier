acdd7994e2fb063fa64501a883944ca24a0d5fd8
InPRTx <inprtx@gmail.com>
2023-05-15 23:00:41 +0800

    Add anycast (#6)
    * add 第一版
    
    * fix 名字
    
    * mod anycast bird 配置

---
+++ anycast/enterpoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+ip addr add 172.23.173.173/29 dev eth0
+ip addr add 172.23.173.174/29 dev eth0
+ip addr add fdf4:56da:a360::173/64 dev eth0
+ip addr add fdf4:56da:a360::174/64 dev eth0
+
+#ip link add dn42dummy0 type dummy
+#ip addr add 172.23.173.173/32 dev dn42dummy0
+#ip addr add 172.23.173.174/32 dev dn42dummy0
+#ip addr add fdf4:56da:a360::173/128 dev dn42dummy0
+#ip addr add fdf4:56da:a360::174/128 dev dn42dummy0
+#ip link set dn42dummy0 up
+
+bird -d
