0f36f6868c48b5621eeb4b468ab9c6d902cf2bdd
InPRTx <inprtx@gmail.com>
2023-05-15 22:04:08 +0800

    mod anycast bird 配置

--- anycast/enterpoint.sh
+++ anycast/enterpoint.sh
@@ -7,5 +7,4 @@ ip addr add 172.23.173.174/29 dev eth0
 ip addr add fdf4:56da:a360::173/64 dev eth0
 ip addr add fdf4:56da:a360::174/64 dev eth0
 
-rm -f /var/run/babeld.pid
-/usr/sbin/babeld -c /etc/babeld.conf
+bird -d
