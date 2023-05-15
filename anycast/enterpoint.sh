4bf72ea22ac1aac65673429d259b18eb1753b8f8
InPRTx <inprtx@gmail.com>
2023-05-15 22:43:16 +0800

    mod anycast bird 配置

--- anycast/enterpoint.sh
+++ anycast/enterpoint.sh
@@ -7,11 +7,11 @@ set -x
 #ip addr add fdf4:56da:a360::173/64 dev eth0
 #ip addr add fdf4:56da:a360::174/64 dev eth0
 
-ip link add dn42dummy0 type dummy
-ip addr add 172.23.173.173/32 dev dn42dummy0
+#ip link add dn42dummy0 type dummy
+#ip addr add 172.23.173.173/32 dev dn42dummy0
 #ip addr add 172.23.173.174/32 dev dn42dummy0
-ip addr add fdf4:56da:a360::173/128 dev dn42dummy0
+#ip addr add fdf4:56da:a360::173/128 dev dn42dummy0
 #ip addr add fdf4:56da:a360::174/128 dev dn42dummy0
-ip link set dn42dummy0 up
+#ip link set dn42dummy0 up
 
 bird -d
